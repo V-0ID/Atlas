dddba261063fc7d074dc8c68a320354f13b61bed
V-0ID <88525373+V-0ID@users.noreply.github.com>
2021-10-17 16:01:45 +1100

    Update Doc.lua

--- Functions/Watermark_Handler/Doc.lua
+++ Functions/Watermark_Handler/Doc.lua
@@ -2,8 +2,8 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Atlas/main/Functions/Watermark_Handler/init.lua"))();
 
 --Creates WaterMark [Anim]
-function AtlasWaterMark(FunctionDelay, PullText[true/false])
+AtlasWaterMark(FunctionDelay, PullText[true/false])
 --Disables the logo [Anim]
-function DisableLogo(ActionDelay)
+DisableLogo(ActionDelay)
 --Destroys Logo
-function DestroyLogo(DestroyDelay)
+DestroyLogo(DestroyDelay)
