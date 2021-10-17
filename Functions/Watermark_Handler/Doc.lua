deb51c3559579e152e943fa2eb5d4ac976f4e959
V-0ID <88525373+V-0ID@users.noreply.github.com>
2021-10-17 16:01:11 +1100

    Update Doc.lua

--- Functions/Watermark_Handler/Doc.lua
+++ Functions/Watermark_Handler/Doc.lua
@@ -1,3 +1,6 @@
+--LoadScript
+loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Atlas/main/Functions/Watermark_Handler/init.lua"))();
+
 --Creates WaterMark [Anim]
 function AtlasWaterMark(FunctionDelay, PullText[true/false])
 --Disables the logo [Anim]
