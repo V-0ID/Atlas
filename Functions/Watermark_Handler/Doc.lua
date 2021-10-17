28a39ec499dbf4aab98dfd7f7ca13ec8492fda50
V-0ID <88525373+V-0ID@users.noreply.github.com>
2021-10-17 16:00:16 +1100

    Create Doc.lua

---
+++ Functions/Watermark_Handler/Doc.lua
@@ -0,0 +1,6 @@
+--Creates WaterMark [Anim]
+function AtlasWaterMark(FunctionDelay, PullText[true/false])
+--Disables the logo [Anim]
+function DisableLogo(ActionDelay)
+--Destroys Logo
+function DestroyLogo(DestroyDelay)
