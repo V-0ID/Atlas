38f511814a2cdf75344621139e6c4add398b6ea9
V-0ID <88525373+V-0ID@users.noreply.github.com>
2021-10-29 09:48:18 +1100

    Create Loader.lua

---
+++ Api/Loader.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 286090429 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Atlas/main/Api/Games/Arsenal.lua"))();
+elseif game.PlaceId == 621129760 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Atlas/main/Api/Games/KAT.lua"))();
+else
+warn("Your Current Game Isn't Supported")
+end
