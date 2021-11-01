cdf4b706b09d22593096924e154c575651cfd17b
V-0ID <88525373+V-0ID@users.noreply.github.com>
2021-11-01 14:35:57 +1100

    Update Loader.lua

--- Api/Loader.lua
+++ Api/Loader.lua
@@ -2,6 +2,8 @@ if game.PlaceId == 286090429 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Atlas/main/Api/Games/Arsenal.lua"))();
 elseif game.PlaceId == 621129760 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Atlas/main/Api/Games/KAT.lua"))();
+elseif game.PlaceId == 1962086868 then 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Atlas/main/Api/Games/TOH.lua"))();
 else
 warn("Your Current Game Isn't Supported")
 end
