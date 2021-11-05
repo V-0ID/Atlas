ea675433744524bed36335103c3fc4d416b4c4a4
V-0ID <88525373+V-0ID@users.noreply.github.com>
2021-11-05 19:20:34 +1100

    Update Loader.lua

--- Api/Loader.lua
+++ Api/Loader.lua
@@ -4,6 +4,12 @@ elseif game.PlaceId == 621129760 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Atlas/main/Api/Games/KAT.lua"))();
 elseif game.PlaceId == 1962086868 then 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Atlas/main/Api/Games/TOH.lua"))();
+elseif game.PlaceId == 3527629287 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/V-0ID/Atlas/main/Api/Games/BigPaintball.lua"))();
 else
-warn("Your Current Game Isn't Supported")
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Game Not Supported";
+    Text = "The game you are playing isn't supported! Please check the discord to see the supported games";
+    Duration = "7";
+})
 end
